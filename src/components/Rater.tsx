b6aa86403c1eaad9541b690d0415b9f5e6c11451
enriquepalaciosv <palacios335@gmail.com>
2024-02-22 19:23:28 -0600

    Render new competitor for evaluation

--- src/components/Rater.tsx
+++ src/components/Rater.tsx
@@ -1,32 +1,71 @@
-import React from "react";
-import { List, Space, Typography, Rate, Image } from "antd";
-import { getAvatar } from '../utils/avatar-util';
-import { Competitor } from "../types/Competitor";
+import React, { useCallback, useEffect, useState } from "react";
+import { List, Space, Typography, Rate, Image, Flex, Spin } from "antd";
+import { getAvatar } from "../utils/avatar-util";
+import { useFirebase } from "../hooks/useFirebase";
 
 export const Rater: React.FC = () => {
+  const [current, setCurrent] = useState<any>();
+  const [allCompetitors, setAllCompetitors] = useState<any[]>();
+  const [avatar, setAvatar] = useState<string>();
+  const { getAllCompetitors } = useFirebase();
+  const data = ["Divertido", "Original", "A mano", "Maquillaje", "Pasarela"];
 
-  const data = [
-    'Divertido',
-    'Original',
-    'A mano',
-    'Maquillaje',
-    'Pasarela',
-  ];
+  const watchNew = (collection?: any[]) => {
+    if (collection) {
+      const found = collection?.find((el) => el.status === "new");
+      if (found) {
+        setCurrent(found);
+        setAvatar(getAvatar(found.type));
+      } else {
+        setCurrent(undefined);
+        setAvatar(getAvatar());
+      }
+    }
+  };
 
-  const avatar = getAvatar(Competitor.Group)
-  const current = 'Nombre del disfraz';
+  const loadCompetitors = useCallback(async () => {
+    const all = await getAllCompetitors();
+    if (all) {
+      watchNew(all);
+      setAllCompetitors(all);
+    }
+  }, [getAllCompetitors]);
+
+  useEffect(() => {
+    loadCompetitors();
+  }, [loadCompetitors]);
+
+  useEffect(() => {
+    watchNew(allCompetitors);
+  }, [allCompetitors]);
+
+  if (!current) {
+    return (
+      <Flex align="center" gap="middle" style={{ height: "100vh" }}>
+        <Spin size="large" />
+      </Flex>
+    );
+  }
 
   return (
-    <Space direction="vertical" size="middle" style={{ display: 'flex', alignItems: 'center' }}>
+    <Space
+      direction="vertical"
+      size="middle"
+      style={{ display: "flex", alignItems: "center" }}
+    >
       <Image src={avatar} width={200} />
-      <Typography.Title level={3} style={{ textAlign: 'center' }}>{current}</Typography.Title>
+      <Typography.Title level={3} style={{ textAlign: "center" }}>
+        {current.name}
+      </Typography.Title>
       <List
         bordered
         dataSource={data}
         renderItem={(item) => (
           <List.Item>
-            <Typography.Text style={{ marginRight: '8px', fontSize: '1.2rem' }}>{item}</Typography.Text>
-            <Rate style={{ fontSize: '28px' }} />
+            <Typography.Text style={{ marginRight: "8px", fontSize: "1.2rem" }}>
+              {item}
+            </Typography.Text>
+            <Rate style={{ fontSize: "28px" }} />
           </List.Item>
         )}
       />
